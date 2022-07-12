9e51f264cf69c1f30014a048801aa5edd1416a5a
Yekpahi Prince <princeyekpahi@hotmail.fr>
2022-07-12 18:26:59 +0200

    third commit

--- base/views.py
+++ base/views.py
@@ -3,8 +3,13 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+projects = [
+    {"id" : 1, "name" : "Flyer evenement"},
+    {"id" : 2, "name" : "Activité politique"},
+    {"id" : 3, "name" : "Royal Fleur"}
+]
 def home(request) :
     return render(request, 'home.html')
 
 def project(request) :
-    return render(request, 'project.html')
+    return render(request, 'project.html',{'projects' : projects} )
