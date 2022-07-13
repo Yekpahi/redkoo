e9d3ecad768bb5a8d37bad3dcc0e12e1e9a3099c
Yekpahi Prince <princeyekpahi@hotmail.fr>
2022-07-13 13:03:44 +0200

    ignore

--- base/views.py
+++ base/views.py
@@ -9,7 +9,8 @@ projects = [
     {"id" : 3, "name" : "Royal Fleur"}
 ]
 def home(request) :
-    return render(request, 'home.html')
+    return render(request, 'base/home.html')
 
 def project(request) :
-    return render(request, 'project.html',{'projects' : projects} )
+    context = {'projects' : projects}
+    return render(request, 'base/project.html',{'projects' : context} )
